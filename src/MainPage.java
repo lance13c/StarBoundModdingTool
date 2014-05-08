8c8953fbb28b069b434297277a99160228823a6f
dpc2061 <dpc2061@rit.edu>
2014-05-08 00:00:59 -0400

    First Impressions of Main Model View and Controller

---
+++ src/MainPage.java
@@ -0,0 +1,30 @@
+/**
+ * File:
+ *  $Id$
+ *
+ * Revisions:
+ *  $Log$
+ */
+/**
+ * Created by Dominic Cicilio on 5/1/2014.
+*/
+
+
+public class MainPage {
+    private MainPageView view;
+    private MainPageModel model;
+    private MainPageController controller;
+
+        //Controls Everything
+    public static void main(String[] arg){
+        MainPage main = new MainPage();
+        main.model = new MainPageModel();
+        main.view = new MainPageView(main.model);
+        main.controller = new MainPageController(main.view,main.model);
+
+        main.view.setVisible(true);
+    }
+
+    public MainPage() {
+    }
+}
