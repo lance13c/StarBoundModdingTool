a4c23a959f279858424c9d8e54141ddfb796e73d
dpc2061 <dpc2061@rit.edu>
2014-05-30 21:13:19 -0400

    Parsing Libraries Added

--- src/JavaParserClass.java
+++ src/JavaParserClass.java
@@ -4,10 +4,31 @@
 import javax.json.Json;
 import javax.json.stream.JsonParser;
 import java.io.StringReader;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JavaParserClass {
+    private final String DIR_TEST = "C:\\Program Files " +
+            "(x86)\\Steam\\SteamApps\\common\\Starbound\\extracted assets\\recipes\\level33\\tier6door.recipe";
+
 
     public static void main(String[] args){
-        JsonParser parser = Json.createParser(new StringReader("[]"));
+        JavaParserClass javaParser = new JavaParserClass();
+        JSONParser parser = new JSONParser();
+        System.out.println("======Parsing======");
+
+        try {
+            Object obj = parser.parse(javaParser.DIR_TEST);
+
+        }catch (ParseException e){
+            e.getStackTrace();
+        }finally {
+            //parser.
+        }
+
+    }
+
+    public JavaParserClass(){
+
     }
 }
